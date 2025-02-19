f6f648433479a0b626bf29dca3ec225f6b503c7e
Daniel Paulino <160649011+thedanielpaulino@users.noreply.github.com>
2025-02-19 09:59:23 -0300

    Update login.php
    tirei a parte html do php

--- login.php
+++ login.php
@@ -12,25 +12,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("Location: pagina.html"); // Redireciona para a página protegida
         exit;
     } else {
-        $erro = "Usuário ou senha incorretos!";
+        // Redireciona de volta ao login com erro
+        header("Location: login.html?erro=1");
+        exit;
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login Luma</title>
-</head>
-<body>
-    <h2>Login</h2>
-    <?php if (isset($erro)) echo "<p style='color:red;'>$erro</p>"; ?>
-    <form method="post">
-        <label>Usuário:</label>
-        <input type="text" name="username" required><br><br>
-        <label>Senha:</label>
-        <input type="password" name="password" required><br><br>
-        <input type="submit" value="Entrar">
-    </form>
-</body>
-</html>
